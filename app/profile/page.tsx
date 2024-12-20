ce2c584323a1afc747ce0e1f615018cd5fa0a3f9
enbraining <me@bricn.net>
2024-12-21 00:05:13 +0900

    css 수정

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -49,26 +49,45 @@ export default function Page() {
 					자격증
 				</SelectMenu>
 			</div>
-			{menu === "LINK" ? (
-				<div className="mt-5 grid gap-y-2">
-					<LineLink type="GITHUB" href={"https://github.com/enbraining"}>
-						Github
-					</LineLink>
-					<LineLink type="LINKEDIN" href={"https://linkedin.com/in/enbraining"}>
-						LinkedIn
-					</LineLink>
-					<LineLink
-						type="SOLVEDAC"
-						href={"https://solved.ac/profile/enbraining"}
-					>
-						Solved.ac
-					</LineLink>
+			<div>
+				{menu === "LINK" ? (
+					<div className="mt-5 grid gap-y-2">
+						<LineLink type="GITHUB" href={"https://github.com/enbraining"}>
+							Github
+						</LineLink>
+						<LineLink
+							type="LINKEDIN"
+							href={"https://linkedin.com/in/enbraining"}
+						>
+							LinkedIn
+						</LineLink>
+						<LineLink
+							type="SOLVEDAC"
+							href={"https://solved.ac/profile/enbraining"}
+						>
+							Solved.ac
+						</LineLink>
+					</div>
+				) : (
+					<div className="mt-5 grid gap-y-2">
+						<LineTitle
+							content="전자산업기사"
+							date="2024년 12월 24일 결과 발표"
+						/>
+					</div>
+				)}
+			</div>
+			{/* <div className="mt-10">
+				<p className="text-xl font-bold text-neutral-800">학력</p>
+				<hr className="mt-2 mb-8" />
+				<div>
+					<div>
+						<h1 className="font-medium text-lg">광주소프트웨어마이스터고등학교</h1>
+						<h2>스마트IoT과</h2>
+                        <p className="text-neutral-500 text-sm">2023년 3월 – ?</p>
+					</div>
 				</div>
-			) : (
-				<div className="mt-5 grid gap-y-2">
-					<LineTitle content="전자산업기사" date="2024년 12월 24일 결과 발표" />
-				</div>
-			)}
+			</div> */}
 		</div>
 	);
 }
