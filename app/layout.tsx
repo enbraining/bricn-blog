a2f439431cb5387d15b41334d042f672666f0a5f
enbraining <me@bricn.net>
2025-01-05 18:47:36 +0900

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import LocalFont from "next/font/local";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./globals.css";
+import GoogleAnalytics from "./lib/googleAnalytics";
 
 const localFont = LocalFont({
 	src: "../public/fonts/PretendardVariable.woff2",
@@ -21,12 +22,18 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-            <head>
-                {/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5938651528318065"
-        crossOrigin="anonymous"></script>
-            </head>
+			<head>
+				{/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5938651528318065"
+					crossOrigin="anonymous"
+				/>
+			</head>
 			<body className={`antialiased ${localFont.className}`}>
+				{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+					<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+				)}
 				<ThemeProvider>
 					<div className="lg:mx-[16rem] md:mx-[6rem] mx-[1rem]">
 						<div className="min-h-[92vh]">
---
+++ app/lib/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/script';
+
+export default function GoogleAnalytics({ gaId }: { gaId: string }) {
+	return (
+		<>
+			<Script
+				async
+				src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+			/>
+			<Script
+				id="google-analytics"
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+				dangerouslySetInnerHTML={{
+					__html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${gaId}');
+                    `,
+				}}
+			/>
+		</>
+	);
+}
