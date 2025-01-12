7c85d527e678ca97e561167ffa9c22cb0a43edb6
enbraining <me@bricn.net>
2025-01-12 23:10:02 +0900

    type

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -4,9 +4,8 @@ async function getPost(id: string){
     return (await supabase()).from('posts').select('*').eq('id', id).single();
 }
 
-export async function GET(request: Request, response: Response, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
-
     const { data } = await getPost(id);
 
     return new Response(JSON.stringify(data), {
