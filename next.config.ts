2e2ca6e613e18ca20b34779aeb41524dd4065068
enbraining <me@bricn.net>
2025-01-16 20:44:29 +0900

    cors

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { getBaseUrl } from "./app/lib/url";
 
 const nextConfig: NextConfig = {
 	images: {
@@ -10,7 +11,19 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
-    crossOrigin: "anonymous"
+    headers: async () => {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: getBaseUrl() },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Post-Category" },
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
