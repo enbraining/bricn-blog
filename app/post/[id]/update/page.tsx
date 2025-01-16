b53b5d114c08faa138b3e39cae1e88b1de9b5578
enbraining <me@bricn.net>
2025-01-16 21:04:12 +0900

    post

--- app/post/[id]/update/page.tsx
+++ app/post/[id]/update/page.tsx
@@ -41,7 +41,7 @@ export default function Page({ params }: { params: Promise<{ id: string }> }) {
     const onChangeContent = useCallback((value?: string) => {
         setPost((prev) => ({
             ...prev!,
-            content: value || ""
+            content: value || "???"
        }))
     }, [])
 
