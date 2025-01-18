5d4968f4c7023ac079f84c7a736abfe040a98381
enbraining <me@bricn.net>
2025-01-19 01:57:12 +0900

    login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,42 +1,38 @@
 "use client"
 
+import { Session } from "@supabase/supabase-js";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
 
 export default function Page(){
-    const [session, setSession] = useState(false)
-
-    const onSignIn = useCallback(() => {
-        redirect("/auth/signin")
-    }, [])
+    const [session, setSession] = useState<Session | null>(null)
 
     const onSignOut = useCallback(async () => {
         await supabase.auth.signOut()
+        redirect("/auth/signin")
     }, [])
 
     useEffect(() => {
         const fetchSession = async () => {
             const { data } = await supabase.auth.getSession()
-            if(data.session) setSession(!!data.session)
+            setSession(data.session)
+
+            if (!data.session) {
+                redirect("/auth/signin");
+            }
         }
+
         fetchSession()
     }, [])
 
     return (
         <div className="grid">
-            <div className="mx-auto grid gap-y-2">
-                {
-                    session
-                        ? (
-                            <div className="grid gap-y-2">
-                                <button className="border px-52 py-3 rounded-sm" onClick={onSignOut}>로그아웃</button>
-                                <Link className="border px-52 py-3 rounded-sm" href="/post/new">작성하기</Link>
-                            </div>
-                        )
-                        : <button className="border px-52 py-3 rounded-sm" onClick={onSignIn}>로그인</button>
-                }
+            <div className="mx-auto grid gap-y-2 text-center">
+                    <h1>{session?.user.email}</h1>
+                    <button className="border px-52 py-3 rounded-sm" onClick={onSignOut}>로그아웃</button>
+                    <Link className="border px-52 py-3 rounded-sm" href="/post/new">작성하기</Link>
             </div>
         </div>
     )
--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -2,9 +2,11 @@
 
 import { supabase } from "@/app/lib/supabase"
 import Form from "next/form"
+import { useRouter } from "next/navigation"
 import { useCallback } from "react"
 
 export default function Page(){
+    const router = useRouter()
     const onSubmit = useCallback((formData: FormData) => {
         const body = {
             email: formData.get("email") as string,
@@ -16,13 +18,16 @@ export default function Page(){
         }
 
         fetchToken()
-    }, [])
+        router.push("/auth")
+    }, [router])
 
     return (
-        <Form action={onSubmit}>
-            <input type="email" name="email"></input>
-            <input type="password" name="password"></input>
-            <button type="submit"></button>
-        </Form>
+        <div className="grid">
+            <Form action={onSubmit} className="mx-auto grid gap-y-2">
+                <input className="border rounded-sm p-2" type="email" name="email"></input>
+                <input className="border rounded-sm p-2" type="password" name="password"></input>
+                <button className="border rounded-sm p-2" type="submit">로그인</button>
+            </Form>
+        </div>
     )
 }
