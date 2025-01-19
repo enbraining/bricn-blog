a785454316b2cbbf0f3e6b59f00fd7a5970c8b44
enbraining <me@bricn.net>
2025-01-19 18:00:30 +0900

    unified

--- app/components/content/MarkdownContent.tsx
+++ app/components/content/MarkdownContent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import 'katex/dist/katex.min.css';
@@ -8,13 +9,14 @@ import rehypeKatex from 'rehype-katex';
 import rehypePrismPlus from 'rehype-prism-plus';
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+import { Pluggable } from 'unified';
 
 export default function MarkdownContent({ content }: { content: string }) {
 	return (
 		<div className="lg:mx-[17rem] md:mx-[0rem] sm:mx-[0rem]">
             <Markdown
                 remarkPlugins={[remarkGfm, remarkMath]}
-                rehypePlugins={[rehypeKatex, rehypePrismPlus]}
+                rehypePlugins={[rehypeKatex, rehypePrismPlus as Pluggable]}
                 className="markdown-body"
             >
                 {content}
