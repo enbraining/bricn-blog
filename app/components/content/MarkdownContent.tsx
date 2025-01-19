e6e848f41997f30f38d4cce65cf072578ceffbc3
enbraining <me@bricn.net>
2025-01-19 17:49:01 +0900

    code style

--- app/components/content/MarkdownContent.tsx
+++ app/components/content/MarkdownContent.tsx
@@ -1,36 +1,21 @@
 "use client";
 
+import 'katex/dist/katex.min.css';
+import 'prism-themes/themes/prism-vsc-dark-plus.css';
+
 import Markdown from "react-markdown";
-import SyntaxHighlighter from "react-syntax-highlighter";
+import rehypeKatex from 'rehype-katex';
+import rehypePrismPlus from 'rehype-prism-plus';
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 
 export default function MarkdownContent({ content }: { content: string }) {
 	return (
 		<div className="lg:mx-[17rem] md:mx-[0rem] sm:mx-[0rem]">
             <Markdown
-                components={{
-                    code({ inline, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-
-                        if (!inline && match) {
-                            return (
-                                <SyntaxHighlighter {...props} language={match[1]} PreTag="div">
-                                    {String(children).replace(/\n$/, "")}
-                                </SyntaxHighlighter>
-                            );
-                        }
-                        return (
-                            <code
-                                {...props}
-                                className={`${className || ""} inline-code text-[#905]`}
-                            >
-                                {children}
-                            </code>
-                        );
-                    },
-                }}
-                remarkPlugins={[remarkGfm]}
-                className="markdown-content"
+                remarkPlugins={[remarkGfm, remarkMath]}
+                rehypePlugins={[rehypeKatex, rehypePrismPlus]}
+                className="markdown-body"
             >
                 {content}
             </Markdown>
