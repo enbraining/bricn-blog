89f6346404fc40cc7fad3defd3ed455f9af854e3
enbraining <me@bricn.net>
2025-01-21 12:16:40 +0900

    post

--- app/components/content/MarkdownContent.tsx
+++ app/components/content/MarkdownContent.tsx
@@ -15,7 +15,7 @@ export default function MarkdownContent({ content }: { content: string }) {
             <Markdown
                 remarkPlugins={[remarkGfm, remarkMath]}
                 rehypePlugins={[rehypeKatex, rehypePrismPlus as Pluggable]}
-                className="markdown-body"
+                className="markdown-body overflow-x-auto"
             >
                 {content}
             </Markdown>
