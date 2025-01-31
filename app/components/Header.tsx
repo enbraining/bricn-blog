9f9a0cfaa007b2623dd1fe0b0012870a7ee5e0c7
enbraining <me@bricn.net>
2025-02-01 00:39:18 +0900

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,12 +3,21 @@
 import { IconMenu } from '@tabler/icons-react';
 import { useState } from 'react';
 import HoverLink from './basic/HoverLink';
+import Link from 'next/link';
 
 export default function Header() {
   const [isToggle, setToggle] = useState(false);
 
   return (
-    <header className="sticky top-0 py-6 z-10 flex">
+    <header className="sticky top-0 py-6 z-10 flex items-center">
+      <div>
+        <Link
+          className="font-semibold hover:text-bricn-300 duration-300 text-bricn-100"
+          href={'/'}
+        >
+          Bricn Tech Blog
+        </Link>
+      </div>
       <div className="ml-auto flex text-bricn-200 items-center gap-x-6">
         <HoverLink href={'/portfolio'}>Portfolio</HoverLink>
         <HoverLink href={'/'}>Blog</HoverLink>
