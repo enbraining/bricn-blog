ba8c3c0a6a4334c8bd408fa2653dac7152c61004
enbraining <me@bricn.net>
2025-02-01 09:24:27 +0900

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,18 +9,18 @@ export default function Header() {
   const [isToggle, setToggle] = useState(false);
 
   return (
-    <header className="sticky top-0 py-6 z-10 flex items-center">
+    <header className="sticky top-0 py-6 z-10 flex items-center w-full">
       <Link
         className="sm:block hidden font-semibold hover:text-bricn-300 duration-300 text-bricn-100"
         href={'/'}
       >
         Bricn Tech Blog
       </Link>
-      <div className="ml-auto flex text-bricn-200 items-center gap-x-6">
+      <div className="sm:ml-auto sm:w-min w-full ml-0 flex text-bricn-200 items-center gap-x-6">
         <HoverLink href={'/portfolio'}>Portfolio</HoverLink>
         <HoverLink href={'/'}>Blog</HoverLink>
         <HoverLink href={'/links'}>Links</HoverLink>
-        <div onClick={() => setToggle(!isToggle)} className="ml-2">
+        <div onClick={() => setToggle(!isToggle)} className="ml-auto">
           <IconMenu size={24} strokeWidth={1.5} />
         </div>
       </div>
