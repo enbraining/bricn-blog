36aa1b5df298b558d15f762d4511b31fc6b34281
enbraining <me@bricn.net>
2025-02-27 09:45:40 +0900

    tag page

--- app/post/tag/page.tsx
+++ app/post/tag/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { supabase } from '@/app/lib/supabase';
 import { Tag } from '@/app/types/Tag';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Page() {
   const [tags, setTags] = useState<Tag[]>([]);
-  const router = useRouter();
 
   useEffect(() => {
     const fetchTags = async () => {
@@ -18,24 +17,17 @@ export default function Page() {
     fetchTags();
   }, []);
 
-  const onClickHandler = useCallback(
-    (name: string) => {
-      router.push(`/post?category=${name}`);
-    },
-    [router]
-  );
-
   return (
     <div>
       <div className="mb-3 cursor-grab flex-wrap select-none gap-2 whitespace-nowrap">
         {tags.map((c) => (
-          <div
-            onClick={() => onClickHandler(c.name)}
+          <Link
+            href={`/?category=${c.name}`}
             className={`hover:text-bricn-100 text-bricn-300  px-2 py-1 rounded-sm`}
             key={c.name}
           >
             <p className="uppercase">{`${c.name} (${c.count})`}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
