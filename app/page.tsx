fdd1dea7cc4e44a22524d698b7bc2ef58b412eb1
enbraining <me@bricn.net>
2025-03-26 15:03:24 +0900

    optimize mobile size

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export default function Page() {
   return (
-    <div className="grid gap-y-8">
+    <div>
       <section className="font-bold text-xl text-neutral-400">
         <div className="mt-8">
           <p>
@@ -15,16 +15,15 @@ export default function Page() {
             시도해보기를 즐깁니다.
           </p>
         </div>
-        <div className="mt-8">
-          <Image
-            src={'/images/mc_fnd.png'}
-            alt="mc_fnd"
-            width={600}
-            height={100}
-            className="w-full h-fit rounded-sm aspect-video object-cover"
-          />
-        </div>
-        <hr className="border-neutral-200 mt-8" />
+        <Image
+          src={'/images/mc_fnd.png'}
+          alt="mc_fnd"
+          width={1600}
+          height={900}
+          priority={true}
+          className="mt-8 w-fit rounded-sm aspect-video object-cover"
+        />
+        <hr className="border-neutral-200 my-8" />
       </section>
 
       <section className="font-bold text-xl text-neutral-400">
@@ -35,7 +34,7 @@ export default function Page() {
             이해하고 프로젝트에 적용할 수 있습니다.
           </a>
         </p>
-        <hr className="border-neutral-200 mt-8" />
+        <hr className="border-neutral-200 my-8" />
       </section>
 
       <section className="font-bold text-xl text-neutral-400">
@@ -48,7 +47,7 @@ export default function Page() {
           <a className="text-neutral-600">T/C</a> 등의 내장 기능을 이용해서,
           원하는 기능을 구현할 수 있습니다.
         </p>
-        <hr className="border-neutral-200 mt-8" />
+        <hr className="border-neutral-200 my-8" />
       </section>
 
       <section className="font-bold text-xl text-neutral-400">
@@ -56,15 +55,13 @@ export default function Page() {
           무선통신에 흥미가 있고, 시간적인 여유만 생긴다면 아마추어무선기사를
           공부할 예정입니다.
         </p>
-        <hr className="border-neutral-200 mt-8" />
+        <hr className="border-neutral-200 my-8" />
       </section>
 
-      <section className="font-bold text-xl text-neutral-400">
+      <section className="font-bold text-xl text-neutral-400 w-fit">
         <p>사물놀이를 비롯한 아름다운 소리, 음악 등을 좋아합니다.</p>
         <iframe
-          className="mt-4"
-          width="560"
-          height="315"
+          className="mt-4 w-full aspect-video"
           src="https://www.youtube.com/embed/J-V7cUvkX3s?si=aLy6zIJ6iJkO1laR"
           title="YouTube video player"
           frameBorder="0"
