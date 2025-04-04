3c90ed7e899511828515aed695dba6d122d77ace
enbraining <me@bricn.net>
2025-04-04 16:09:22 +0900

    update header position & styles

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,20 +1,36 @@
 'use client';
 
-import { IconBlockquote, IconBriefcase, IconHome2 } from '@tabler/icons-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Header() {
+  const [page, setPage] = useState<'HOME' | 'PUBLICATION' | 'PORTFOLIO'>(
+    'HOME'
+  );
+
   return (
     <header className="fixed py-4 flex items-center bottom-0 w-full">
-      <div className="grid grid-flow-col text-bricn-200 items-center gap-x-6 mx-auto px-12 py-3 rounded-full bg-neutral-100 bg-opacity-70">
-        <Link href={'/'}>
-          <IconHome2 />
+      <div className="text-bricn-200 items-center mx-auto text-center sm:w-1/2 w-5/6 grid grid-flow-col">
+        <Link
+          href={'/'}
+          className={`text-center border py-3 bg rounded-l-full bg-white ${page === 'HOME' && 'bg-black text-white border-none'}`}
+          onClick={() => setPage('HOME')}
+        >
+          Home
         </Link>
-        <Link href={'/post'}>
-          <IconBlockquote />
+        <Link
+          href={'/post'}
+          className={`text-center border-y bg-white py-3 ${page === 'PUBLICATION' && 'bg-black text-white border-none'}`}
+          onClick={() => setPage('PUBLICATION')}
+        >
+          Publication
         </Link>
-        <Link href={'/portfolio'}>
-          <IconBriefcase />
+        <Link
+          href={'/portfolio'}
+          className={`text-center bg-white border rounded-r-full py-3 ${page === 'PORTFOLIO' && 'bg-black text-white border-none'}`}
+          onClick={() => setPage('PORTFOLIO')}
+        >
+          Portfolio
         </Link>
       </div>
     </header>
