1c1d0361de4405f0ededd6aad349db90fe4dd341
enbraining <me@bricn.net>
2025-06-12 14:15:16 +0900

    update header

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,30 +1,14 @@
-import {
-  IconBrandGithub,
-  IconBrandLinkedin,
-  IconBrandYoutube,
-} from '@tabler/icons-react';
 import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <header className="grid grid-flow-col items-center py-5">
+    <footer className="grid grid-flow-col items-center py-5">
       <Link
         href={'https://github.com/enbraining/Bricn'}
         className="text-neutral-600"
       >
         ⓒ 2024. enbraining All rights reserved.
       </Link>
-      <div className="flex ml-auto gap-x-2 items-center text-neutral-600">
-        <Link href={'https://github.com/enbraining'}>
-          <IconBrandGithub size={25} />
-        </Link>
-        <Link href={'https://linkedin.com/in/enbraining'}>
-          <IconBrandLinkedin size={25} />
-        </Link>
-        <Link href={'https://youtube.com/@enbraining'}>
-          <IconBrandYoutube size={25} />
-        </Link>
-      </div>
-    </header>
+    </footer>
   );
 }
--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,14 +1,29 @@
 'use client';
 
+import {
+  IconAt,
+  IconBrandGithubFilled,
+  IconBrandLinkedinFilled,
+} from '@tabler/icons-react';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 bg-neutral-100 border-neutral-300 border rounded-b-lg py-2 px-14 flex">
-      <div className="mx-auto flex items-center gap-x-12">
-        <Link href={'/'}>소개</Link>
+    <header className="font-semibold my-4 flex items-center">
+      <div className="flex items-center gap-x-12">
+        <Link href={'/'}>메인</Link>
         <Link href={'/post'}>블로그</Link>
-        <Link href={'/feed.xml'}>RSS</Link>
+      </div>
+      <div className="flex ml-auto gap-x-2 items-center text-neutral-600">
+        <Link href={'https://github.com/enbraining'}>
+          <IconBrandGithubFilled size={22} />
+        </Link>
+        <Link href={'https://linkedin.com/in/enbraining'}>
+          <IconBrandLinkedinFilled size={22} />
+        </Link>
+        <Link href={'https://youtube.com/@enbraining'}>
+          <IconAt size={22} />
+        </Link>
       </div>
     </header>
   );
